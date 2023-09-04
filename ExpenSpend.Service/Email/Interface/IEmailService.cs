fa531b56b87be7db3f56cfa5db5efb0db55622be
rahulpromact <rahul@promactinfo.com>
2023-09-04 13:04:08 +0530

    Add email notification when password is chenge

---
+++ ExpenSpend.Service/Email/Interface/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using ExpenSpend.Core.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenSpend.Service.Email.Interface;
+
+public interface IEmailService
+{
+    void SendEmail(MessageDto email);
+    Task<MessageDto> CreateEmailValidationTemplateMessage(string email, string confirmationCode);
+    Task<string> EmailConfirmationPageTemplate();
+    void SendPasswordResetEmail(string recipientEmail, string resetLink);
+    void SandPasswordChangeNotification(string email, string userName);
+}
